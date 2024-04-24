9fa601fc5b6f16cc1219ef984a6ab3eb5a171dd3
Megavolt9000 <megavolt90000@gmail.com>
2024-04-25 00:17:38 +0400

    версия_final_Done_24.04.2024(ver.1)
    Signed-off-by: Megavolt9000 <megavolt90000@gmail.com>

--- Main.java
+++ Main.java
@@ -7,15 +7,12 @@ import ru.netology.graphics.server.GServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
 
         TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
-        ///converter.setMaxHeight(300);
-        /// converter.setMaxWidth(150);
+
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
-        converter.setMaxHeight(150);
-        converter.setMaxWidth(75);
+
         // Или то же, но с выводом на экран:
         //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
         //String imgTxt = converter.convert(url);
